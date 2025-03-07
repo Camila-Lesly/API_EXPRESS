8997e56dcd3ced9a406336306c0c4777584dc6b5
Sergio <sergio.velarde@alumnos.uneatlantico.es>
2025-03-07 14:23:19 +0100

    Cambios service middleware y controller para la funcion de login. Faltaria declarar la ruta

--- modules/auth/auth.service.js
+++ modules/auth/auth.service.js
@@ -1,26 +1,42 @@
 (function () {
     'use strict';
 
-    module.exports = {
-        updateUser: updateUser
-    };
-
+    var bcrypt = require('bcrypt');
+    var jwt = require('jsonwebtoken');
     var UserModel = require('./auth.module')().UserModel;
+    var config = require('../config'); // Archivo donde tengas tu SECRET_KEY
+
+    module.exports = {
+        updateUser: updateUser,
+        loginUser: loginUser
+    };
 
     function updateUser(userId, user) {
         if (user.password) {
             user.password = hashPassword(user.password);
         }
         return UserModel
-            .findByIdAndUpdate(userId, user, {new: true})
+            .findByIdAndUpdate(userId, user, { new: true })
             .exec();
     }
+
+    function loginUser(email, password) {
+        return UserModel.findOne({ email }).exec()
+            .then(user => {
+                if (!user || !comparePassword(password, user.password)) {
+                    throw new Error('Credenciales inválidas');
+                }
+                var token = jwt.sign({ id: user._id, email: user.email }, config.SECRET_KEY, { expiresIn: '1h' });
+                return { token, user };
+            });
+    }
+
+    function hashPassword(password) {
+        return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+    }
+
+    function comparePassword(password, hash) {
+        return bcrypt.compareSync(password, hash);
+    }
+
 })();
-
-function hashPassword(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-}
-
-function comparePassword(password, hash) {
-    return bcrypt.compareSync(password, hash);
-}
