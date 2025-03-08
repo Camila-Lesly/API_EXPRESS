f0d33d060489bf605c3d5aaf976c2e25a30c57ea
ian saucedo <iamasterpro1@gmail.com>
2025-03-08 20:58:01 +0100

    implementacion delete

--- modules/auth/auth.middleware.js
+++ modules/auth/auth.middleware.js
@@ -1,25 +1,68 @@
-(function () {
-    'use strict';
+;(function () {
+  'use strict'
 
-    module.exports = {
-        updateUser: updateUser
-    };
-   
-    var AuthService = require('./auth.module')().AuthService;
+  module.exports = {
+    updateUser: updateUser,
+    deleteProfile: deleteProfile,
+    verifyToken: verifyToken
+  }
 
-function updateUser(req, res, next) {
-        AuthService.updateUser(req.params.userId, req.body)
-            .then(success)
-            .catch(error);
+  var AuthService = require('./auth.module')().AuthService
+  var jwt = require('jsonwebtoken')
+  const config = require('../../config/config')
 
-        function success(data) {
-            req.response = data;
-            next();
-        }
+  function updateUser(req, res, next) {
+    AuthService.updateUser(req.params.userId, req.body).then(success).catch(error)
 
-        function error(err) {
-            next(err);
-        }
+    function success(data) {
+      req.response = data
+      next()
     }
 
-})();
+    function error(err) {
+      next(err)
+    }
+  }
+
+  function deleteProfile(req, res, next) {
+    AuthService.deleteProfile(req.user.id).then(success).catch(error)
+
+    function success(data) {
+      req.response = data
+      next()
+    }
+
+    function error(err) {
+      err.status = err.status || 500
+      next(err)
+    }
+  }
+
+  function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization']
+
+    if (!bearerHeader) {
+      const error = new Error('No token provided')
+      error.status = 401
+      return next(error)
+    }
+
+    const [bearer, token] = bearerHeader.split(' ')
+
+    if (bearer !== 'Bearer' || !token) {
+      const error = new Error('Invalid token format')
+      error.status = 401
+      return next(error)
+    }
+
+    jwt.verify(token, config.jwtSecret, (err, decoded) => {
+      if (err) {
+        const error = new Error('Invalid token')
+        error.status = 401
+        return next(error)
+      }
+      req.user = decoded
+      next()
+    })
+  }
+})()
--- modules/auth/auth.service.js
+++ modules/auth/auth.service.js
@@ -1,26 +1,41 @@
-(function () {
-    'use strict';
+;(function () {
+  'use strict'
 
-    module.exports = {
-        updateUser: updateUser
-    };
+  module.exports = {
+    updateUser: updateUser,
+    deleteProfile: deleteProfile
+  }
 
-    var UserModel = require('./auth.module')().UserModel;
+  var UserModel = require('./auth.module')().UserModel
 
-    function updateUser(userId, user) {
-        if (user.password) {
-            user.password = hashPassword(user.password);
-        }
-        return UserModel
-            .findByIdAndUpdate(userId, user, {new: true})
-            .exec();
+  function updateUser(userId, user) {
+    if (user.password) {
+      user.password = hashPassword(user.password)
     }
-})();
+    return UserModel.findByIdAndUpdate(userId, user, { new: true }).exec()
+  }
+
+  function deleteProfile(userId) {
+    return UserModel.findByIdAndDelete(userId)
+      .exec()
+      .then(deletedUser => {
+        if (!deletedUser) {
+          const error = new Error('User not found')
+          error.status = 404
+          throw error
+        }
+        return {
+          message: 'Profile deleted successfully',
+          user: deletedUser
+        }
+      })
+  }
+})()
 
 function hashPassword(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
 }
 
 function comparePassword(password, hash) {
-    return bcrypt.compareSync(password, hash);
+  return bcrypt.compareSync(password, hash)
 }
