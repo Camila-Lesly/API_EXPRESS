3bdec05724ea362ceb344c976d048e53a122ed2c
ian saucedo <iamasterpro1@gmail.com>
2025-03-08 21:07:36 +0100

    WITHOUT JSWTOKEN

--- modules/auth/auth.middleware.js
+++ modules/auth/auth.middleware.js
@@ -3,66 +3,32 @@
 
   module.exports = {
     updateUser: updateUser,
-    deleteProfile: deleteProfile,
-    verifyToken: verifyToken
+    deleteProfile: deleteProfile
   }
 
-  var AuthService = require('./auth.module')().AuthService
-  var jwt = require('jsonwebtoken')
-  const config = require('../../config/config')
+  const AuthService = require('./auth.module')().AuthService
 
-  function updateUser(req, res, next) {
-    AuthService.updateUser(req.params.userId, req.body).then(success).catch(error)
-
-    function success(data) {
+  async function updateUser(req, res, next) {
+    try {
+      const data = await AuthService.updateUser(req.params.userId, req.body)
       req.response = data
       next()
-    }
-
-    function error(err) {
-      next(err)
-    }
-  }
-
-  function deleteProfile(req, res, next) {
-    AuthService.deleteProfile(req.user.id).then(success).catch(error)
-
-    function success(data) {
-      req.response = data
-      next()
-    }
-
-    function error(err) {
+    } catch (err) {
       err.status = err.status || 500
       next(err)
     }
   }
 
-  function verifyToken(req, res, next) {
-    const bearerHeader = req.headers['authorization']
-
-    if (!bearerHeader) {
-      const error = new Error('No token provided')
-      error.status = 401
-      return next(error)
-    }
-
-    const [bearer, token] = bearerHeader.split(' ')
-
-    if (bearer !== 'Bearer' || !token) {
-      const error = new Error('Invalid token format')
-      error.status = 401
-      return next(error)
-    }
-
-    jwt.verify(token, config.jwtSecret, (err, decoded) => {
-      if (err) {
-        const error = new Error('Invalid token')
-        error.status = 401
-        return next(error)
-      }
-      req.user = decoded
+  async function deleteProfile(req, res, next) {
+    try {
+      // For now, get userId directly from params instead of JWT token
+      const userId = req.params.userId
+      const data = await AuthService.deleteProfile(userId)
+      req.response = data
       next()
-    })
+    } catch (err) {
+      err.status = err.status || 500
+      next(err)
+    }
   }
 })()
